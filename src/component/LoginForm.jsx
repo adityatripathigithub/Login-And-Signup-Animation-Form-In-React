a709c5e53482f62826ffd1f4a89d0ccf423c4938
adity tripathi <tripathiaditya163@gmail.com>
2024-11-13 22:11:21 +0530

    make a formate

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -8,6 +8,7 @@ const LoginForm = () => {
             <form className="flex justify-center pt-[25vh] items-center flex-row gap-10 ">
                 <h3>Enter Your Name</h3>
                 <input className="w-[20vw] h-[5vh] text-black" type="text" placeholder="Enter Your User Name" />
+
                 <h3>Enter Your Password</h3>
                 <input className="w-[20vw] h-[5vh] text-black" type="Password" placeholder="Enter Your Password" />
 
